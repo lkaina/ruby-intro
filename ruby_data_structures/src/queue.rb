fcca8f0d362cfa5c04e1af295e337b6c30e17739
unknown <Leig@Leig-PC.mit.edu>
2013-10-23 01:10:41 -0700

    implemented queue, not yet tested.

--- ruby_data_structures/src/queue.rb
+++ ruby_data_structures/src/queue.rb
@@ -1,4 +1,24 @@
 class Queue
+  # attr_accessor :length
+
   def initialize
+    @length = 0
+    @storage = []
+  end
+
+  def length
+    @length
+  end
+
+  def add item
+    @storage << item
+    @length += 1
+  end
+
+  def remove
+    unless @length <= 0
+      @length -= 1
+      @storage.shift
+    end
   end
 end
